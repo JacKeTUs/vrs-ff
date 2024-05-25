5f6b22464775bcc66d3b0403c50cf104c39b4416
Oleg <oleg@makarenk.ooo>
2024-05-26 01:15:25 +0300

    Fix START_DELAY undefined

--- hid-pidff.c
+++ hid-pidff.c
@@ -737,7 +737,7 @@ static void pidff_autocenter(struct pidff_device *pidff, u16 magnitude)
 	pidff->set_effect[PID_TRIGGER_REPEAT_INT].value[0] = 0;
 	pidff_set(&pidff->set_effect[PID_GAIN], magnitude);
 	pidff->set_effect[PID_DIRECTION_ENABLE].value[0] = 1;
-	pidff->set_effect[PID_START_DELAY].value[0] = 0;
+	//pidff->set_effect[PID_START_DELAY].value[0] = 0;
 
 	hid_hw_request(pidff->hid, pidff->reports[PID_SET_EFFECT],
 			HID_REQ_SET_REPORT);
